48cf1d31dec3a7ad963f6fb5911b51f84e4d02a4
Caspian <caspian.noren@gmail.com>
2018-11-02 15:57:23 +0100

    Revert "Added another movie"
    This reverts commit 3ebde08e1423350323c209980f336c7a77a8f6e7.

--- Checkpoint4/Checkpoint4/Program.cs
+++ Checkpoint4/Checkpoint4/Program.cs
@@ -9,14 +9,10 @@ namespace Checkpoint4
             Console.WriteLine("Hello World!");
             Console.WriteLine("Best movies ever!");
             Console.WriteLine("Copyright Caspian Berggren Norén 2018 :)");
-<<<<<<< HEAD
 
             Console.WriteLine("Instant Action");
             Console.WriteLine("Terminal Termination");
             Console.WriteLine("Inferno of Termination");
-=======
-            Console.WriteLine("Terminal Termination");
->>>>>>> Added movie "Terminal Termination"
         }
     }
 }
