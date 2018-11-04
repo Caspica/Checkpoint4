101878693c22a467cc141df972e4f6af776ef254
Caspian <caspian.noren@gmail.com>
2018-11-04 22:23:48 +0100

    Update Checkpoint4/Checkpoint4/Program.cs

--- Checkpoint4/Checkpoint4/Program.cs
+++ Checkpoint4/Checkpoint4/Program.cs
@@ -11,9 +11,9 @@ namespace Checkpoint4
             Console.WriteLine("Copyright Caspian Berggren Norén 2018 :)");
             
 
+            Console.WriteLine("Inferno of Termination");
             Console.WriteLine("Instant Action");
             Console.WriteLine("Terminal Termination");
-            Console.WriteLine("Inferno of Termination");
         }
     }
 }
